7fcb3ddc57552011fd5a7d174e01c26d18c920fc
Himadri Purkait <purkaithimadricollege@gmail.com>
2024-07-06 13:08:21 +0530

    sidebar close initialy

--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const appSlice = createSlice({
   name: "app",
   initialState: {
-    sidebar: true,
+    sidebar: false,
   },
   reducers: {
     toggleSidebar: (state) => {
